28f7dab7c7637bc09bca21d889d8c6ae6bc12678
yuvaraj_yuvi <ddontknowbro@gmail.com>
2026-07-04 14:10:51 +0530

    fixed render deploy

--- main.py
+++ main.py
@@ -4,11 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return """
-    <h1>🎓 Campus Buddy</h1>
-    <h3>Website is running successfully 🚀</h3>
-    <p>Created by Yuvaraj</p>
-    """
+    return "Campus Buddy Running Successfully 🚀"
 
 if __name__ == "__main__":
     app.run(debug=True)
